ef9e54c0c9684c4bc1a97b126c86d55b172f688b
Will Kessler <willkessler@gmail.com>
2024-09-13 16:47:21 -0700

    added AI compliance page starting

---
+++ src/routes/manage/ai/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+ import { Button } from "$lib/components/ui/button";
+ import { Badge } from "$lib/components/ui/badge";
+ import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-svelte";
+ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "$lib/components/ui/table";
+</script>
+
+<h1 class="text-3xl font-bold mb-6">AI Compliance Checker</h1>
+
+<h3>Clear and Compliant can run an organization-wide check of all your compliance concerns using our custom AI. Just click the button to see how you're doing.</h3>
+
+<Button class="mt-4" style="background-color:#44f">Run AI Check Now</Button>
