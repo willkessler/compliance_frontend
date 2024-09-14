d4f97d0ea0c620536a53b062d971617a8a0e57da
Will Kessler <willkessler@gmail.com>
2024-09-14 12:23:00 -0700

    first pass at sidebar rework

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -2,6 +2,13 @@
  import { page } from '$app/stores';
  import { Separator } from "$lib/components/ui/separator";
  import { LayoutDashboard, ScrollText, Building2, Truck, Users, Link, CircleAlert, Info, WandSparkles, Settings } from "lucide-svelte";
+ import { ChartPieSolid, GridSolid, MailBoxSolid, UserSolid, ShoppingBagSolid, ArrowRightToBracketOutline, 
+        EditOutline, FireSolid, BookSolid, RestoreWindowOutline, LifeSaverSolid } from 'flowbite-svelte-icons';
+
+ import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, SidebarDropdownWrapper, SidebarDropdownItem } from 'flowbite-svelte';
+ let spanClass = 'flex-1 ms-3 whitespace-nowrap';
+
+ $: activeUrl = $page.url.pathname;
 
  const menuItems = [
    { href: '/', label: 'Dashboard', icon: LayoutDashboard, color: 'text-inherit' },
@@ -19,44 +26,61 @@
    ]
    },
  ];
+
+ const adminItems = [
+   { href: '/manage/ai', label: 'AI-Compliance Check', icon: WandSparkles, color: 'text-inherit', sideLabel: 'New!' },
+   { href: '/manage/integrations', label: 'Connections', icon: Link, color: 'text-inherit' },
+   { href: '/manage/organization', label: 'Admin', icon: Settings , color: 'text-inherit' },
+   { href: '/help', label: 'Help', icon: Info, color: 'text-inherit' },
+ ];
+ 
+  let activeClass = 'flex items-center p-2 text-base font-normal text-primary-900 bg-primary-200 dark:bg-dark-gray-700 rounded-lg dark:text-white hover:bg-primary-100 dark:hover:bg-gray-700';
+  let nonActiveClass = 'flex items-center p-2 text-base font-normal text-green-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-green-700';
+
 </script>
 
-<aside class="w-64 bg-card text-card-foreground border-r flex flex-col">
-  <div class="p-5 text-xl font-bold">Trucking Co.</div>
-  <nav class="flex-grow pl-2">
-    {#each menuItems as item}
-      <a 
-        href={item.href} 
-        class="flex items-center px-4 py-2 hover:bg-accent {item.color}"
-        class:bg-accent={$page.url.pathname === item.href}
-        >
-        <svelte:component this={item.icon} class="mr-2" size={20} />
-        {item.label}
-        {#if item.pillCount !== undefined}
-          <span class="ml-auto bg-orange-600 text-white text-xs font-semibold px-2 py-1 rounded-full">{item.pillCount}</span>
-        {/if}     
-      </a>
-      {#if item.subItems}
-        {#each item.subItems as subItem}
-          <a 
-            href={subItem.href} 
-            class="flex items-center px-4 py-2 pl-12 hover:bg-accent text-sm"
-            class:bg-accent={$page.url.pathname.startsWith(subItem.href)}
-          >
-            {subItem.label}
-          </a>
-        {/each}
-      {/if}
-    {/each}
-  </nav>
-  <Separator class="w-[90%] mx-auto my-4" />
-  <div class="mt-auto ml-3 mb-2 text-sm">
-    <a href="/manage/ai" class="flex items-center px-4 py-2 hover:bg-accent text-blue-500"><WandSparkles class="mr-2" size={20} />
-      AI-Compliance Check
-      <span class="ml-auto text-sm text-blue-400">New!</span>
-    </a>
-    <a href="/manage/integrations" class="flex items-center px-4 py-2 hover:bg-accent"><Link class="mr-2" size={20} /> Setup Connections</a>
-    <a href="/manage/organization" class="flex items-center px-4 py-2 hover:bg-accent"><Settings class="mr-2" size={20} /> Admin</a>
-    <a href="#" class="flex items-center px-4 py-2 hover:bg-accent mb-4"><Info class="mr-2" size={20} /> Help</a>
-  </div>
-</aside>
+<Sidebar {activeUrl} {activeClass} {nonActiveClass}>
+  <SidebarWrapper>
+    <div class="ml-3 mt-4 mb-4 text-xl font-bold">Trucking Co.</div>
+    <SidebarGroup>
+      {#each menuItems as item}
+        {#if item.subItems !== undefined }
+          <SidebarDropdownWrapper label={item.label} isOpen={false}>
+            <svelte:fragment slot="icon">
+              <svelte:component this={item.icon} class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+            </svelte:fragment>
+            {#each item.subItems as subItem}
+              <SidebarDropdownItem class="pl-12" label={subItem.label} href={subItem.href} />
+            {/each}
+          </SidebarDropdownWrapper>
+        {/if}
+        {#if item.subItems == undefined }
+          <SidebarItem label={item.label} href={item.href}>
+            <svelte:fragment slot="icon">
+              <svelte:component this={item.icon} class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+            </svelte:fragment>
+            <svelte:fragment slot="subtext">
+              {#if item.pillCount !== undefined}
+                <span class="inline-flex justify-center items-center p-3 ms-3 w-3 h-3 text-sm font-medium text-primary-600 bg-primary-200 rounded-full dark:bg-primary-900 dark:text-primary-200"> {item.pillCount} </span>
+              {/if}
+            </svelte:fragment>
+          </SidebarItem>
+        {/if}
+      {/each}
+    </SidebarGroup>
+    <SidebarGroup border>
+      {#each adminItems as item}
+        <SidebarItem label={item.label}>
+          <svelte:fragment slot="icon">
+            <svelte:component this={item.icon} class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+          </svelte:fragment>
+          <svelte:fragment slot="subtext">
+            {#if item.sideLabel !== undefined}
+              <span class="inline-flex items-center p-3 ms-3 w-3 h-3 text-sm font-medium text-blue-600"> {item.sideLabel} </span>
+            {/if}
+          </svelte:fragment>
+        </SidebarItem>
+      {/each}
+    </SidebarGroup>
+  </SidebarWrapper>
+</Sidebar>
