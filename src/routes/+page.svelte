a7598b231f09c6de0587c8d4c1f935e7dec9c401
Will Kessler <willkessler@gmail.com>
2024-09-20 09:29:19 -0700

    moved news items into a datafile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
-<!-- Main Dashboard -->
-
 <script lang="ts">
  import { onMount, afterUpdate } from 'svelte';
  import { Card, CardContent, CardHeader, CardTitle } from "$lib/components/ui/card";
@@ -11,6 +9,7 @@
  import ComplianceHistoryChart from '$lib/components/ComplianceHistoryChart.svelte';
  import DonutChart from '$lib/components/DonutChart.svelte';
  import ChatBot from  '$lib/components/ChatBot.svelte';
+ import { newsItems } from '$lib/data/newsItemsData';
 
  const actionItems = [
    { type: "Federal Filings", openItems: 3, criticalItems:2, url: '/manage/filings/federal' },
@@ -18,47 +17,6 @@
    { type: "Vehicles", openItems: 8, criticalItems:0, url: '/manage/fleet/drivers' },
    { type: "Drivers", openItems: 13, criticalItems:0, url: '/manage/fleet/vehicles' },
    { type: "Connections", openItems: 1, criticalItems:0, url: '/manage/integrations' },
- ];
-
- interface NewsItem {
-   title: string;
-   image: string;
-   url: string;
-  }
-
- const newsItems: NewsItem[] = [
-    {
-      title: 'California Supreme Court upholds AB5 exemption for gig workers',
-      subtitle: 'Case on independent contractor law involved workers’ comp and ballot initiative Prop 22 that granted carve-out',
-      synopsis: 'Uber and Lyft drivers in California will continue to be protected from the state\’s AB5 independent contractor classification law following a decision by the state’s Supreme Court.',
-      site: 'Freightwaves',
-      image: 'https://www.freightwaves.com/uploads/2024/07/25/021223-76-1200x675.jpg',
-      url: 'https://www.freightwaves.com/news/california-supreme-court-upholds-ab5-exemption-for-gig-workers',
-    },
-    {
-      title: 'From voluntary to mandatory: Consultative expertise in sustainability compliance',
-      subtitle: 'Sustainability reporting is quickly shifting from a voluntary initiative to a mandatory undertaking',
-      synopsis: 'Governments and consumers alike have placed an increased emphasis on environmental concerns over the past several years. As a result, sustainability reporting is quickly shifting from a voluntary initiative to a mandatory undertaking. Strong relationships with transportation and logistics providers are going to be more important than ever as shippers learn to work within the regulations and expectations that come along with this “new normal."',
-      site: 'Freightwaves',
-      image: '/images/news/consultative.png',
-      url: 'https://www.freightwaves.com/news/from-voluntary-to-mandatory-consultative-expertise-in-sustainability-compliance',
-    },
-    {
-      title: '4 steps to improving compliance and operations in 2023',
-      subtitle: 'New regulations and proposals shouldn’t be the only things on fleets\’ minds this year',
-      synopsis: 'Compliance and operations work hand in hand, and this year, fleets have put improving in both categories on their New Year’s resolutions lists.',
-      site: 'Freightwaves',
-      image: 'https://www.freightwaves.com/uploads/2023/02/22/Petro_Joplin_44_Aerial-155_IF-1-1200x674.jpg',
-      url: 'https://www.freightwaves.com/news/4-steps-to-improving-compliance-and-operations-in-2023',
-    },
-    {
-      title: 'CARB\'s Clean Truck Check testing pushed back to 2025',
-      subtitle: 'This delay is only for required inspections, meaning fleets and owner/operators are still expected to register and pay fees.',
-      synopsis: 'Noregon reported that The California Air Resources Board (CARB) announced that it has pushed back the date for required Clean Truck Check Act inspections until January 1, 2025. This delay is only for required inspections, meaning fleets and owner/operators are still expected to register and pay fees.',
-      site: 'Fleet Equipment Magazine',
-      image: 'https://marvel-b1-cdn.bc0a.com/f00000000269980/s18391.pcdn.co/wp-content/uploads/2019/01/truck-lights-generic.jpg',
-      url: 'https://www.fleetequipmentmag.com/carbs-clean-truck-check-delayed/',
-    },
  ];
 
  const cardStyle = "height: 500px; display: flex; flex-direction: column; overflow: hidden";
