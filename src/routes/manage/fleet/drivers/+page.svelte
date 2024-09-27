cea098be8eb9f38e6629da0ddf1c42c2e2b84fae
Will Kessler <willkessler@gmail.com>
2024-09-27 13:49:00 -0700

    cleaned up padding on vehicles and drivers lists

--- src/routes/manage/fleet/drivers/+page.svelte
+++ src/routes/manage/fleet/drivers/+page.svelte
@@ -87,47 +87,48 @@
   </div>
 </div>
 
-<Table class="relative overflow-x-auto sm:rounded-lg mt-5 ml-0" hoverable>
-  <TableHead class=" bg-customGray/15 whitespace-nowrap">
-    <TableHeadCell class="px-6 py-3 text-xs font-medium text-customGray uppercase">Name</TableHeadCell>
-    <TableHeadCell class="px-6 py-3 text-xs font-medium text-customGray uppercase">Driving Status</TableHeadCell>
-    <TableHeadCell class="px-6 py-3 text-xs font-medium text-customGray uppercase">Total drive time</TableHeadCell>
-    <TableHeadCell class="px-6 py-3 text-xs font-medium text-customGray uppercase">Total miles driven</TableHeadCell>
-    <TableHeadCell class="px-6 py-3 text-xs font-medium text-customGray uppercase">Status</TableHeadCell>
-    <TableHeadCell class="px-6 py-3 text-xs font-medium text-customGray uppercase">Take Action</TableHeadCell>
-  </TableHead>
-  <TableBody class="bg-white divide-y divide-gray-200">
-    {#each drivers as driver}
-      <TableBodyRow class="cursor-pointer" on:click={() => navigateToDriverDetails(driver.id)}>
-        <TableBodyCell class="px-6 py-4 whitespace-nowrap text-sm font-large text-customGray">{driver.name}</TableBodyCell>
-        <TableBodyCell class="px-6 py-4 whitespace-nowrap text-sm font-large text-customGray">
-          <Badge large rounded class="bg-{getStatusColor(driver.status)}-200 px-2 py-1.5 rounded rounded-[6px] min-w-32">
-            {getDriverStatus(driver)}
-          </Badge>
-        </TableBodyCell>
-        <TableBodyCell class="px-6 py-4 whitespace-nowrap text-sm font-large text-customGray">{driver.totalDriveTime}</TableBodyCell>
-        <TableBodyCell class="px-6 py-4 whitespace-nowrap text-sm font-large text-customGray">{driver.totalMiles}</TableBodyCell>
-        <TableBodyCell>
-          <Badge large rounded class="text-customGray bg-{getStatusColor(driver.status)}-200 px-2 py-1.5 rounded rounded-[6px] min-w-32">
+<div class="ml-4 mr-4">
+  <Table class="relative overflow-x-auto sm:rounded-lg mt-5" hoverable>
+    <TableHead class=" bg-customGray/15 whitespace-nowrap">
+      <TableHeadCell class="px-2 py-3 text-xs font-medium text-customGray uppercase">Name</TableHeadCell>
+      <TableHeadCell class="px-2 py-3 text-xs font-medium text-customGray uppercase">Driving Status</TableHeadCell>
+      <TableHeadCell class="px-2 py-3 text-xs font-medium text-customGray uppercase">Total drive time</TableHeadCell>
+      <TableHeadCell class="px-2 py-3 text-xs font-medium text-customGray uppercase">Total miles driven</TableHeadCell>
+      <TableHeadCell class="px-2 py-3 text-xs font-medium text-customGray uppercase">Status</TableHeadCell>
+      <TableHeadCell class="px-2 py-3 text-xs font-medium text-customGray uppercase">Take Action</TableHeadCell>
+    </TableHead>
+    <TableBody class="bg-white divide-y divide-gray-200">
+      {#each drivers as driver}
+        <TableBodyRow class="cursor-pointer" on:click={() => navigateToDriverDetails(driver.id)}>
+          <TableBodyCell class="px-2 py-4 whitespace-nowrap text-sm font-large text-customGray">{driver.name}</TableBodyCell>
+          <TableBodyCell class="px-2 py-4 whitespace-nowrap text-sm font-large text-customGray">
+            <Badge large rounded class="bg-{getStatusColor(driver.status)}-200 px-2 py-1.5 rounded rounded-[6px] min-w-32">
+              {getDriverStatus(driver)}
+            </Badge>
+          </TableBodyCell>
+          <TableBodyCell class="px-2 py-4 whitespace-nowrap text-sm font-large text-customGray">{driver.totalDriveTime}</TableBodyCell>
+          <TableBodyCell class="px-2 py-4 whitespace-nowrap text-sm font-large text-customGray">{driver.totalMiles}</TableBodyCell>
+          <TableBodyCell class="px-2 py-4 whitespace-nowrap text-sm font-large text-customGray">
+            <Badge large rounded class="text-customGray bg-{getStatusColor(driver.status)}-200 px-2 py-1.5 rounded rounded-[6px] min-w-32">
               {#if driver.icon !== undefined}
                 <svelte:component this={driver.icon} class="mr-2 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
               {/if}
-            {upgradeStatus(driver)}
-          </Badge>
-        </TableBodyCell>
-        <TableBodyCell class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-          <Button
-            on:click={() => navigateToDriverDetails(driver.id)}
-            color="light" 
-            class="text-customGray hover:text-customGray p-2 min-w-32">
-            See details →
-          </Button>
-        </TableBodyCell>
-      </TableBodyRow>
-    {/each}
-  </TableBody>
-</Table>
-
+              {upgradeStatus(driver)}
+            </Badge>
+          </TableBodyCell>
+          <TableBodyCell class="px-2 py-4 whitespace-nowrap text-sm font-medium">
+            <Button
+              on:click={() => navigateToDriverDetails(driver.id)}
+              color="light" 
+              class="text-customGray hover:text-customGray p-2 min-w-32">
+              See details →
+            </Button>
+          </TableBodyCell>
+        </TableBodyRow>
+      {/each}
+    </TableBody>
+  </Table>
+</div>
 
 <div class="w-full flex justify-end pr-4 mt-4">
   <Pagination {pages} on:previous={previous} on:next={next} icon>
