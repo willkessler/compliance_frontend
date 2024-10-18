32f27ba231759ace85d7d57c30fb40cf5683045e
Will Kessler <willkessler@gmail.com>
2024-10-17 20:33:00 -0700

    now created modals for all kinds of activities

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -39,8 +39,8 @@
    { href: 'https://chat.regulis.ai', newTab: true, disabled: false, label: 'AI-Compliance Check',
      icon: WandSparkles, color: 'text-inherit', sideLabel: 'New!' },
    { href: '/manage/integrations', newTab: false, disabled: false, label: 'Integrations', icon: ShareNodesOutline, color: 'text-inherit' },
-   { href: '/help', newTab: false, label: 'Help', disabled: true, icon: QuestionCircleOutline, color: 'text-inherit' },
    { href: '/manage/organization', newTab: false, disabled: false, label: 'Admin', icon: Settings , color: 'text-inherit' },
+   { href: '/help', newTab: false, label: 'Help', disabled: true, icon: QuestionCircleOutline, color: 'text-inherit' },
  ];
  
  let activeClass = 'flex items-center p-2 text-base font-normal text-gray-900 bg-gray-200 dark:bg-gray-700  dark:text-white hover:bg-gray-300 dark:hover:bg-gray-300';
